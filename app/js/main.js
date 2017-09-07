11fb997a030902e488dea680586aaa7a95f04421
isosky <wangtr@asialantao.com>
2017-09-07 10:43:58 +0800

    去掉树插件中的折叠功能

--- app/js/main.js
+++ app/js/main.js
@@ -37,18 +37,6 @@ var init_tree = function() {
         // end
         $(this).toggleClass('selector');
     });
-    // $('.tree li.parent_li > span').on('click', function(e) {
-    //     var children = $(this).parent('li.parent_li').find(' > ul > li');
-    //     // console.log($(this));
-    //     if (children.is(":visible")) {
-    //         children.hide('fast');
-    //         $(this).find(' > i').addClass('icon-plus-sign').removeClass('icon-minus-sign');
-    //     } else {
-    //         children.show('fast');
-    //         $(this).find(' > i').addClass('icon-minus-sign').removeClass('icon-plus-sign');
-    //     }
-    //     e.stopPropagation();
-    // });
 };
 
 function productchild(data, _root) {
